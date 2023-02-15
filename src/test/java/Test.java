539499cddca428cff4f49f8097c78cb5bb5ab24b
VolodymyrDrobko <volodymyr.drobko@gmail.com>
2023-02-15 14:48:18 +0200

    initial commit

---
+++ src/test/java/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(System.getenv("username"));
+        System.out.println(System.getenv("password"));
+    }
+}
