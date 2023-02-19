17b7d3d4cf54219c1c0e66bcffb0ef6c649b5d20
VolodymyrDrobko <volodymyr.drobko@gmail.com>
2023-02-19 11:00:07 +0200

    Add suiteName parameter

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(System.getenv("username"));
         System.out.println(System.getenv("password"));
+        System.out.println(System.getenv("suiteName"));
     }
 }
